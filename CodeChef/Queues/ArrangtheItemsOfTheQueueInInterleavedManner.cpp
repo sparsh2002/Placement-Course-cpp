f56913e28280a64d490247450f9e0414eae7c270
Sparsh Jhariya <sparshjhariya@Sparshs-MacBook-Air.local>
2021-12-18 22:39:20 +0530

    mastering queue session 3 completed

--- CodeChef/Queues/ArrangtheItemsOfTheQueueInInterleavedManner.cpp
+++ CodeChef/Queues/ArrangtheItemsOfTheQueueInInterleavedManner.cpp
@@ -16,10 +16,56 @@ double EPS = 1e-9;
 const ll N = 10000000;
 double PI = acos(-1);
 
+void solve(vector<int> &arr , int n , int k){
+    queue <int> q;
+    stack <int> st;
+    for(int i=0 ; i<n ; i++){
+        q.push(arr[i]);
+    }
+    int x,y;
+    for(int i=0 ; i<k ; i++){
+        x = q.front();
+        q.pop();
+        st.push(x);
+    }
+    while(!st.empty()){
+        x = st.top();
+        st.pop();
+        q.push(x);
+    }
+
+    for(int i=0 ; i<k ; i++){
+        x = q.front();
+        q.pop();
+        st.push(x);
+    }
+    for(int i=0 ; i<n ; i++){
+        if(i%2==0){
+            arr[i] = st.top();
+            st.pop();
+        }
+        else{
+            arr[i] = q.front();
+            q.pop();
+        }
+    }
+
+    reverse(arr.begin() , arr.end());
+    
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    
-
+    int n , k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for(int i=0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    solve(arr, n , k);
+    for(int i=0 ; i<n ; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
