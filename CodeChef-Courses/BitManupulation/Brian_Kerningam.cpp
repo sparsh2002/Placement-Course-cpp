788b6e900decf8d834a1e075d0810df32c965549
Sparsh Jhariya <ui20ec62@iiitsurat.ac.in>
2021-12-21 10:17:32 +0530

    mastering bit manupulation session 2 ends

---
+++ CodeChef-Courses/BitManupulation/Brian_Kerningam.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int ct=0;
+    while(n!=0){
+        n = n&(n-1);
+        ct++;
+    }
+    cout << ct << "\n";
+    return 0;
+}
