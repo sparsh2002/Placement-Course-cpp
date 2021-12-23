d0b01e1b35688801d050b48ecd79b581bd90dd14
Sparsh Jhariya <ui20ec62@iiitsurat.ac.in>
2021-12-23 17:38:48 +0530

    alterations of binary search

---
+++ Other-Courses/Aditya-Verma-Courses/BinarySearch/12_finding_the_floor_of_the element_in_a_sorted_array.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+int findFloor(int * arr , int n , int key){
+    int low = 0, high = n-1;
+    int res = -1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(key > arr[mid]){
+            res = mid;
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key;
+    int ans = findFloor(arr , n ,key);
+    cout << ans << "\n";
+
+    return 0;
+}
