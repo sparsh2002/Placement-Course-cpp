d08b66375c649717f3370cab5fedde4ff638d371
Sparsh Jhariya <ui20ec62@iiitsurat.ac.in>
2022-01-06 18:25:40 +0530

    heap playlist started

---
+++ Other-Courses/Aditya-Verma-Courses/Heap/1_kth_smallest_element.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+int KthSmallest(vector<int> arr , int k){
+    priority_queue<int> maxh;
+    for(int i=0 ; i<arr.size() ; i++){
+        maxh.push(arr[i]);
+        if(maxh.size()>k){
+            maxh.pop();
+        }
+    }
+    return maxh.top();
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >>k ;
+    cout << KthSmallest(arr , k) << "\n";
+
+    return 0;
+}
