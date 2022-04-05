7fa7acc76d7f62664ebb1864fcc1223fd2ecdf99
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-04-05 10:56:28 +0530

    dfs of graph

--- Other-Courses/Strivers_graph_series/2_DFS.cpp
+++ Other-Courses/Strivers_graph_series/2_DFS.cpp
@@ -16,39 +16,21 @@ double EPS = 1e-9;
 const ll N = 10000000;
 double PI = acos(-1);
 
-vector<int> bfsOfGraph(int v , vector<vector<int> > &adj){
-    vector<int> bfs;
-    vector<int> vis(v+1 , 0);
 
-    for(int i=1 ; i<=v ; i++){
-        if(!vis[i]){
-            queue<int> q;
-            q.push(i);
-            vis[i] = 1;
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                bfs.push_back(node);
-
-
-                for(int j=0 ; j<adj[node].size() ; j++){
-                    if(!vis[adj[node][j]]){
-                        q.push(adj[node][j]);
-                        vis[adj[node][j]] = 1;
-                    }
-                }
-            }
+void dfsOfGraph(int node , vector<vector<int> >  &adj , vector<bool> &vis , vector<int> &dfs){
+    dfs.push_back(node);
+    vis[node] = true;
+    for(int i=0 ; i<adj[node].size() ; i++){
+        if(!vis[adj[node][i]]){
+            dfsOfGraph(adj[node][i] , adj, vis , dfs);
         }
     }
-
-    return bfs;
 }
 
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
     vector<vector<int> > adj;
 
     int n;
@@ -70,11 +52,17 @@ int main(){
         adj.push_back(temp);
     }
 
-    vector<int> ans =  bfsOfGraph(n ,adj );
+
+    vector<bool> vis(n+1 , false);
+    vector<int> dfs;
+    
+    for(int i=1 ; i<=n ; i++){
+        if(!vis[i])dfsOfGraph(i ,adj  , vis , dfs);
+    }
 
 
-    for(int i=0 ; i<ans.size(); i++){
-        cout << ans[i] << " ";
+    for(int i=0 ; i<dfs.size(); i++){
+        cout << dfs[i] << " ";
     }
 
     return 0;
