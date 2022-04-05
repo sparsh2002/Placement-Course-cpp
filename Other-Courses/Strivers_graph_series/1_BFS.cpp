716b33137701f9042ece1468edde36114fc1bf94
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-04-05 10:34:09 +0530

    strivers graph series started

---
+++ Other-Courses/Strivers_graph_series/1_BFS.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+// static int n = 7;
+// static vector<int> adj[8] = {
+// 		{},
+// 		{2},
+// 		{1,3,7},
+// 		{2,5},
+// 		{6},
+// 		{3,7},
+// 		{4},
+// 		{2,5}
+// };
+
+vector<int> bfsOfGraph(int v , vector<vector<int> > &adj){
+    vector<int> bfs;
+    vector<int> vis(v+1 , 0);
+
+    for(int i=1 ; i<=v ; i++){
+        if(!vis[i]){
+            queue<int> q;
+            q.push(i);
+            vis[i] = 1;
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+
+
+                for(int j=0 ; j<adj[node].size() ; j++){
+                    if(!vis[adj[node][j]]){
+                        q.push(adj[node][j]);
+                        vis[adj[node][j]] = 1;
+                    }
+                }
+                // for(auto it:adj[node]){
+                //     if(!vis[it]){
+                //         q.push(it);
+                //         vis[it] = 1;
+                //     }
+                // }
+            }
+        }
+    }
+
+    return bfs;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<vector<int> > adj;
+
+    int n;
+    cin >> n;
+
+    vector<int> t;
+    adj.push_back(t);
+
+    for(int i=0 ; i<n ; i++){
+        int x;
+        cin >> x;
+        vector<int> temp;
+        for(int j=0 ; j<x ; j++){
+            int y;
+            cin >> y;
+            temp.push_back(y);
+        }
+
+        adj.push_back(temp);
+    }
+
+    vector<int> ans =  bfsOfGraph(n ,adj );
+
+
+    for(int i=0 ; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
---
+++ Other-Courses/Strivers_graph_series/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World"  << "\n";
+	int n;
+	cin >> n;
+	cout << n << "\n";
+}
