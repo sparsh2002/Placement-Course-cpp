e5466da68a460b170ad5d4c28e1325c3307a2168
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-04-22 16:02:08 +0530

    some maths problems

---
+++ codeforces/30_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int h,m;
+        cin >> h >> m;
+        cout << 1440-(h*60 +m) << "\n";
+    }
+
+    return 0;
+}
---
+++ codeforces/31_Ordinary_Numbers.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        int res = 0;
+        for (ll pw = 1; pw <= n; pw = pw * 10 + 1) {
+            for (int d = 1; d <= 9;  d++) {
+            if (pw * d <= n) {
+                res++;
+            }
+            }
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
