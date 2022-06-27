83b82b9ec273e2eba4cb98ab2ef969aee23f0d78
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-27 13:27:59 +0530

    minimum sum partition

---
+++ Other-Courses/Strivers_DP_series.cpp/15_Minimum_sum_partition.cpp
@@ -0,0 +1,26 @@
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+//     vector<vector<int>> dp(n , vector<int>(k+1 , -1));
+//     return solve(n , k , arr ,dp);
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    return dp[n-1][k];
+}
