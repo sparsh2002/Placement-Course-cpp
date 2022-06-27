261fbac00d7b037c392933a99d5ee53daa947850
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-27 16:04:06 +0530

    minimum sum partition

--- Other-Courses/Strivers_DP_series.cpp/14_Partition_equal_subset_sum.cpp
+++ Other-Courses/Strivers_DP_series.cpp/14_Partition_equal_subset_sum.cpp
@@ -1,29 +1,49 @@
 #include<bits/stdc++.h>
-bool solve(int n , int k , vector<int>&arr , vector<vector<int>>&dp){
-    if(k<0){
-        return false;
-    }
-    if(k==0){
-        return true;
-    }
-    if(n==0){
-        return false;
-    }
-    if(dp[n-1][k]!=-1) return dp[n-1][k];
-    if(arr[n-1]<=k){
-        return dp[n-1][k]=solve(n-1 , k-arr[n-1] , arr,dp)
-            || solve(n-1 , k , arr,dp);
-    }
-    else{
-        return dp[n-1][k] = solve(n-1 , k,arr,dp);
-    }
-}
-bool canPartition(vector<int> &arr, int n)
+int minSubsetSumDifference(vector<int>& arr, int n)
 {
-    int sum = 0;
-    sum = accumulate(arr.begin() , arr.end() , sum);
-    if(sum%2==1) return false;
-    int k = sum/2;
-    vector<vector<int>> dp(n , vector<int>(k+1 , -1));
-	return solve(n , k, arr,dp);
+    int k = 0;
+    k = accumulate(arr.begin() , arr.end() , k);
+	vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    
+    int minn = k;
+    int i=0 , j = k;
+    while(i<=j){
+        if(dp[n-1][i]==1 and dp[n-1][j]==1){
+            minn = min(minn , j-i);
+            i++;
+            j--;
+        }
+        else if(dp[n-1][i]!=1){
+            i++;
+        }
+        else if(dp[n-1][j]!=1){
+            j--;
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+    
+    return minn;
 }
