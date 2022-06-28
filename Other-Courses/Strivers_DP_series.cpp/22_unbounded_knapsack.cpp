d328a5879dd49db0c0cc68a15851079209ca8740
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-28 18:20:01 +0530

    unbounded knapsack

---
+++ Other-Courses/Strivers_DP_series.cpp/22_unbounded_knapsack.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+int solve(int i,int wt,vector<int>&value , vector<int>&weight , vector<vector<int>>&dp){
+    if(wt==0){
+        return 0;
+    }
+    if(wt<0){
+        return INT_MIN;
+    }
+    if(i==0){
+        if(weight[i]<=wt){
+            return value[i] + 
+                solve(i ,wt-weight[i] , value , weight,dp);
+        }
+        return 0;
+    }
+    if(dp[i][wt]!=-1) return dp[i][wt];
+    int notPick = solve(i-1 , wt , value , weight,dp);
+    int pick = INT_MIN;
+    if(weight[i]<=wt){
+        pick = value[i] + solve(i , wt-weight[i] , value , weight,dp);
+    }
+    return dp[i][wt] = max(notPick , pick);
+}
+int unboundedKnapsack(int n, int wt, vector<int> &value, vector<int> &weight)
+{
+    vector<vector<int>> dp(n , vector<int>(wt+1 , -1));
+    return solve(n-1,wt , value , weight,dp);
+}
