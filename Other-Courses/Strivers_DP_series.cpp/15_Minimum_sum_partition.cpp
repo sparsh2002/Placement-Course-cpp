fb1556ae4999cbb2d78d18d656f86a83dc6dd475
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-28 09:13:49 +0530

    update

--- Other-Courses/Strivers_DP_series.cpp/15_Minimum_sum_partition.cpp
+++ Other-Courses/Strivers_DP_series.cpp/15_Minimum_sum_partition.cpp
@@ -1,26 +1,57 @@
-bool subsetSumToK(int n, int k, vector<int> &arr) {
-//     vector<vector<int>> dp(n , vector<int>(k+1 , -1));
-//     return solve(n , k , arr ,dp);
-    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
-    
-    for(int i=0; i<n; i++){
-        dp[i][0] = true;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool subsetSumUtil(int ind, int target, vector < int > & arr, vector < vector 
+< int >> & dp) {
+  if (target == 0)
+    return dp[ind][target]=true;
+
+  if (ind == 0)
+    return dp[ind][target] = arr[0] == target;
+
+  if (dp[ind][target] != -1)
+    return dp[ind][target];
+
+  bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
+
+  bool taken = false;
+  if (arr[ind] <= target)
+    taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
+
+  return dp[ind][target] = notTaken || taken;
+}
+
+int minSubsetSumDifference(vector < int > & arr, int n) {
+
+  int totSum = 0;
+
+  for (int i = 0; i < n; i++) {
+    totSum += arr[i];
+  }
+
+  vector < vector < int >> dp(n, vector < int > (totSum + 1, -1));
+
+  for (int i = 0; i <= totSum; i++) {
+    bool dummy = subsetSumUtil(n - 1, i, arr, dp);
+  }
+
+  int mini = 1e9;
+  for (int i = 0; i <= totSum; i++) {
+    if (dp[n - 1][i] == true) {
+      int diff = abs(i - (totSum - i));
+      mini = min(mini, diff);
     }
-    
-    if(arr[0]<=k)
-        dp[0][arr[0]] = true;
-    
-    for(int ind = 1; ind<n; ind++){
-        for(int target= 1; target<=k; target++){
-            
-            bool notTaken = dp[ind-1][target];
-    
-            bool taken = false;
-                if(arr[ind]<=target)
-                    taken = dp[ind-1][target-arr[ind]];
-        
-            dp[ind][target]= notTaken||taken;
-        }
-    }
-    return dp[n-1][k];
+  }
+  return mini;
+
+}
+
+int main() {
+
+  vector < int > arr = {1,2,3,4};
+  int n = arr.size();
+
+  cout << "The minimum absolute difference is: " << minSubsetSumDifference(arr, n);
+
 }
