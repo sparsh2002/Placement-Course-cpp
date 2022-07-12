b583f9ab38351b9a7be8f401a8ada09978305656
Sparsh Jhariya <sparshjhariya@Sparshs-MacBook-Air.local>
2022-07-12 17:55:07 +0530

    greedy CSES

---
+++ codeforces/binary-search/8_ Save_More _Mice.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, k;
+		cin >> n >> k;
+
+		vector<int> x(k);
+		for (int i = 0; i < k; i++) cin >> x[i];
+		sort(x.rbegin(), x.rend());
+
+		int cnt = 0;
+		long long sum = 0;
+
+		while (cnt < x.size() && sum < x[cnt])
+		{
+			sum += n - x[cnt++];
+		}
+
+		cout << cnt << '\n';
+	}
+
+	return 0;
+}
