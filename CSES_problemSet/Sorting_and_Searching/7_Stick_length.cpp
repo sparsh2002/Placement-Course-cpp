cbd30b1e503dfc61a6f84147fb47d1c240b0577c
Sparsh Jhariya <sparshjhariya@Sparshs-MacBook-Air.local>
2022-07-14 12:52:56 +0530

    some greedy problems

---
+++ CSES_problemSet/Sorting_and_Searching/7_Stick_length.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+int n;
+vl arr;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    arr.resize(n);
+    rep(i , 0, n) cin >> arr[i];
+    sort_v(arr);
+    int mid = arr[n/2];
+    ll sum = 0;
+    rep(i , 0 ,n) sum += abs(arr[i]-mid);
+    cout << sum << "\n";
+    return 0;
+}
---
+++ CSES_problemSet/Sorting_and_Searching/8_Maximun_subarray_sum.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ;i++){
+        cin >> arr[i];
+    }
+    ll currSum = 0;
+    ll maxSum = -INFF;
+    for(int i=0 ; i<n ; i++){
+        currSum +=  arr[i];
+        maxSum = max(maxSum , currSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+        
+    }
+    
+    cout << maxSum << endl;
+    return 0;
+}
