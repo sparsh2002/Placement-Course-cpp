fa0e6aa00cafb81875479cbcb0db7c38138f1eaa
Sparsh Jhariya <sparshjhariya@Sparshs-MacBook-Air.local>
2022-07-16 12:25:29 +0530

    some range queries questions

---
+++ CSES_problemSet/RangeQueries/2_Static_Range_sum_queries.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    vl arr(n);
+    rep(i , 0, n) cin >> arr[i];
+    vl prefix(n);
+    prefix[0] = arr[0];
+    rep(i , 1, n) prefix[i] = arr[i]+ prefix[i-1];
+    rep(i , 0, q){
+        int a , b;
+        cin >> a >> b;
+        a--;b--;
+        if(a==0) cout << prefix[b] << "\n";
+        else cout << prefix[b]-prefix[a-1] << "\n";
+    }
+
+    return 0;
+}
---
+++ Problems/40_4_Maximum_Minimum_Queries.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+
+ll a[N], tree[4*N];
+
+void build(int node , int st , int en){
+    if(st==en){
+        tree[node] = a[st];
+        return;
+    }
+
+    int mid = (st+en)/2;
+
+    build(2*node , st , mid);
+    build(2*node+1 , mid+1 , en);
+
+    tree[node] =  min(tree[2*node] , tree[2*node+1]);
+}
+
+
+ll query(int node , int st , int en , int l , int r){
+    if(r<st or l>en){
+        return INT_MAX;
+    }
+
+    if(l<=st and en<=r){
+        return tree[node];
+    }
+
+    int mid = (st+en)/2;
+    return min(query(2*node , st, mid, l, r) , query(2*node+1 , mid+1 , en , l, r));
+
+}
+
+void update(int node , int st , int en , int idx , int val){
+    if (st==end)
+    {
+        a[st] = val;
+        tree[node] = val;
+        return;
+    }
+
+    int mid = (st+end)/2;
+    if(idx<=mid){
+        update[2*node , st, mid , idx, val);
+    }
+    else{
+        update[2*node , mid+1, en , idx, val);
+    }
+
+    tree[node] = max(tree[2*node] , tree[2*node+1]);
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n , q;
+    cin >> n >> q;
+    rep(i , 0 , n) cin >> a[i];
+    build(1 , 0 , n-1);
+    rep(i , 0, q){
+        int l , r;
+        cin >> l >> r;
+        l--; r--;
+        cout << query(1, 0, n-1 , l, r) << "\n";
+    }
+    return 0;
+}
