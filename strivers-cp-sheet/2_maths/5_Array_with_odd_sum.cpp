5141c7ae69429557584a3063574907793a2ef2fe
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-10-30 22:01:31 +0530

    some maths problem

---
+++ strivers-cp-sheet/2_maths/5_Array_with_odd_sum.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define sort_vr(arr) sort(arr.rbegin() , arr.rend());
+#define read(arr , n) rep(i , 0 , n) cin >> arr[i]; 
+#define print_v(arr , n) rep(i , 0 , n) cout << arr[i] << ' '; 
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        bool odd  = false;
+        bool even = false;
+        int x;
+        rep(i , 0 , n){
+            cin >> x;
+            if(x&1) odd = true;
+            else even = true;
+        }
+
+        if((odd and even) or (odd and n%2==1)){
+            cout << "YES" << "\n";
+        }
+        else{
+            cout << "NO" << "\n";
+        }
+    }
+
+    return 0;
+}
