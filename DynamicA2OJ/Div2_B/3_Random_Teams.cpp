14f80efcf74e889367833d2a8447b92f779af4ca
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-10-30 22:56:13 +0530

    some div2B problems

---
+++ DynamicA2OJ/Div2_B/3_Random_Teams.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define rep(i,a,b) for(int i=a ; i<b ; i++)
+#define sort_v(arr) sort(arr.begin() , arr.end());
+#define max_el(arr) *max_element(arr.begin() , arr.end());
+#define min_el(arr) *min_element(arr.begin() , arr.end());
+#define sort_vr(arr) sort(arr.rbegin() , arr.rend());
+#define read(arr , n) rep(i , 0 , n) cin >> arr[i]; 
+#define print_v(arr , n) rep(i , 0 , n) cout << arr[i] << ' '; 
+#define rep_rev(i , a, b) for(int i=a ; i>b ; i--)
+
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+const ll N = 10000000;
+double PI = acos(-1);
+int MOD = 1000000007;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll n , m;
+    cin >> n >> m;
+
+    ll maxx = 0;
+    ll minn = 0;
+
+    ll d = n/m;
+    ll r = n%m;
+    if(r==0){
+        minn = m*(d*(d-1)/2);
+    }
+    else{
+        minn= r*(d*(d+1)/2);
+        minn += (m-r)*(d*(d-1)/2);
+    }
+
+    ll rem = n-m+1;
+    maxx = (rem)*(rem-1)/2;
+
+    cout << minn << " " << maxx;
+    return 0;
+}
